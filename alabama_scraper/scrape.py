74c47e8963ca6fc5a12d02c3c7dd5a50b8893719
Chris Hodges <chris.hodges@adtran.com>
2020-03-16 23:47:33 -0500

    Parse counties from Alabama department of health

--- alabama_scraper/scrape.py
+++ alabama_scraper/scrape.py
@@ -1,15 +1,27 @@
 from bs4 import BeautifulSoup
+from datetime import datetime
 import urllib.request, urllib.error, urllib.parse
 
 aldh_covid_url = 'http://www.alabamapublichealth.gov/infectiousdiseases/2019-coronavirus.html'
 
 class AlabamaCovidTableParser:
 
-
   def __init__(self):
     table = self.fetch_table_data()
-    self.parse_table(table)
-    
+    county_names = ['AUTAUGA', 'BALDWIN', 'BARBOUR', 'BIBB', 'BLOUNT', 'BULLOCK', 'BUTLER', 'CALHOUN',
+                 'CHAMBERS', 'CHEROKEE', 'CHILTON', 'CHOCTAW', 'CLARKE', 'CLAY', 'CLEBURNE',
+                 'COFFEE', 'COLBERT', 'CONECUH', 'COOSA', 'COVINGTON', 'CRENSHAW', 'CULLMAN',
+                 'DALE', 'DALLAS', 'DEKALB', 'ELMORE', 'ESCAMBIA', 'ETOWAH', 'FAYETTE', 'FRANKLIN',
+                 'GENEVA', 'GREENE', 'HALE', 'HENRY', 'HOUSTON', 'JACKSON', 'JEFFERSON', 'LAMAR',
+                 'LAUDERDALE', 'LAWRENCE', 'LEE', 'LIMESTONE', 'LOWNDES', 'MACON', 'MADISON',
+                 'MARENGO', 'MARION', 'MARSHALL', 'MOBILE', 'MONROE', 'MONTGOMERY', 'MORGAN',
+                 'PERRY', 'PICKENS', 'PIKE', 'RANDOLPH', 'RUSSELL', 'SHELBY', 'ST CLAIR', 'SUMTER',
+                 'TALLADEGA', 'TALLAPOOSA', 'TUSCALOOSA', 'WALKER', 'WASHINGTON', 'WILCOX',
+                 'WINSTON']
+    county_dict: dict = {county: 0 for county in county_names}
+    county_dict.update(self.parse_table(table))
+    print(county_dict)
+
 
   def fetch_table_data(self):
     response = urllib.request.urlopen(aldh_covid_url)
@@ -17,8 +29,25 @@ class AlabamaCovidTableParser:
     soup = BeautifulSoup(aldh_covid_page, 'html.parser')
     return soup.find_all('table')[0]
 
-  def parse_table(self, table):
-    pass
+  def parse_table(self, table) -> dict:
+    def get_date(table_row) -> datetime:
+      updated_string: str = table_row.find_all('h2')[1].text
+      time_string: str = updated_string.strip('Updated: ').rstrip(' (CT)').replace('.', '')
+      print(time_string)
+      return datetime.strptime(time_string, '%B %d, %Y %I:%M %p')
+
+    def parse_county_row(table_row) -> tuple:
+      key = table_row.find_all('p')[0]
+      value = table_row.find_all('p')[1]
+      return key.text.upper(), value.text
+
+
+
+    table_rows = table.find_all('tr')
+    get_date(table_rows[0])
+    return {county: count for county, count in map(parse_county_row, table_rows[2:-1])}
+
+
 
 
 app = AlabamaCovidTableParser()
